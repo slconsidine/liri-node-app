874b09d326f5284b23d1bf5abcc2b70e6a9f7296
Sam <samlconsidine@gmail.com>
2020-03-01 22:17:53 -0500

    do-what-it-says command functions.

--- liri.js
+++ liri.js
@@ -9,9 +9,180 @@ var spotify = new Spotify(keys.spotify);
 
 // establishes what command should be run
 var command = process.argv[2];
+var nodeArgs = process.argv;
+
+var readRandom = function(argv2, argv3) {
+    if (argv2 === "concert-this") {
+        var bands = "";
+        bands = argv3;
+
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+            bands = bands + "+" + nodeArgs[i];
+            } else {
+            bands += nodeArgs[i];
+            }
+        }
+
+        var queryURL = "https://rest.bandsintown.com/artists/" + bands + "/events?app_id=codingbootcamp";
+
+        axios.get(queryURL).then(
+            function(response) {
+                for (i = 0; i < response.data.length; i++) {
+                    console.log("-------------------------");
+                    console.log("Venue Name: " + response.data[i].venue.name);
+                    console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+                    var eventDate = response.data[i].datetime;
+                    console.log("Event Date: " + moment(eventDate).format("MMMM Do YYYY")
+                    );
+                }
+            })
+        .catch(function(error) {
+            if (error.response) {
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+    } else if (argv2 === "spotify-this-song") {
+        var searchSong = "";
+        searchSong = argv3;
+
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                searchSong = searchSong + " " + nodeArgs[i];
+            } else {
+                searchSong += nodeArgs[i];
+            }
+        }
+      
+        if (searchSong === "") {
+            spotify.search({
+                type: 'track',
+                query: "The+Sign",
+                limit: 1
+            }) .then(function(response) {
+                console.log(response.tracks);
+                // console.log(response.tracks.items[0].artists[0].name);
+                // console.log(response.tracks.items[0].name);
+                // console.log(response.tracks.items[0].external_urls.spotify);
+                // console.log(response.tracks.items[0].album.name);
+                for (i = 0; i < response.tracks.items.length; i++) {
+                    console.log("Artist(s): " + response.tracks.items[i].artists[i].name);
+                    console.log("Song Name: " + response.tracks.items[i].name);
+                    console.log("Preview Link: " + response.tracks.items[i].external_urls.spotify);
+                    console.log("Album Name: " + response.tracks.items[i].album.name);
+                };
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        } else {
+            spotify.search(
+                { 
+                    type: 'track', 
+                    query: searchSong,
+                    limit: 5
+        
+                })
+                .then(function(response) {
+                    // console.log(response.tracks.items[0].artists[0].name);
+                    // console.log(response.tracks.items[0].name);
+                    // console.log(response.tracks.items[0].external_urls.spotify);
+                    // console.log(response.tracks.items[0].album.name);
+                    for (i = 0; i < response.tracks.items.length; i++) {
+                        console.log("-------------------------");
+                        console.log("Artist(s): " + response.tracks.items[i].artists[0].name);
+                        console.log("Song Name: " + response.tracks.items[i].name);
+                        console.log("Preview Link: " + response.tracks.items[i].external_urls.spotify);
+                        console.log("Album Name: " + response.tracks.items[i].album.name);
+                    };
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+            }
+    } else if (argv2 === "movie-this") {
+        var movieTitle = "";
+        movieTitle = argv3;
+
+
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+            movieTitle = movieTitle + "+" + nodeArgs[i];
+            } else {
+            movieTitle += nodeArgs[i];
+            }
+        }
+        console.log(movieTitle);
+
+        if (movieTitle === "") {
+            var queryURL = "https://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy";
+            console.log(queryURL);
+            axios.get(queryURL).then(
+                function(response) {
+                    console.log("Movie Title: " + response.data.Title);
+                    console.log("Release Year " + response.data.Year);
+                    console.log("IMDB Rating: " + response.data.imdbRating);
+                    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                    console.log("Produced in: " + response.data.Country);
+                    console.log("Language: " + response.data.Language);
+                    console.log("Actors: " + response.data.Actors);
+                })
+                .catch(function(error) {
+                if (error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                });
+            } else {
+                var queryURL = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+                axios.get(queryURL).then(
+                    function(response) {
+                        console.log("Movie Title: " + response.data.Title);
+                        console.log("Release Year " + response.data.Year);
+                        console.log("IMDB Rating: " + response.data.imdbRating);
+                        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                        console.log("Produced in: " + response.data.Country);
+                        console.log("Language: " + response.data.Language);
+                        console.log("Actors: " + response.data.Actors);
+                    })
+                    .catch(function(error) {
+                    if (error.response) {
+                        console.log("---------------Data---------------");
+                        console.log(error.response.data);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.status);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log("Error", error.message);
+                    }
+                    });
+                }
+    }
+}
 
 if (command === "concert-this") {
-    var nodeArgs = process.argv;
     var bands = "";
 
     for (var i = 3; i < nodeArgs.length; i++) {
@@ -51,7 +222,6 @@ if (command === "concert-this") {
         console.log(error.config);
     });
 } else if (command === "spotify-this-song") {
-    var nodeArgs = process.argv;
     var searchSong = "";
 
     for (var i = 3; i < nodeArgs.length; i++) {
@@ -68,11 +238,6 @@ if (command === "concert-this") {
               query: "The+Sign",
               limit: 1
           }) .then(function(response) {
-            console.log(response.tracks);
-            // console.log(response.tracks.items[0].artists[0].name);
-            // console.log(response.tracks.items[0].name);
-            // console.log(response.tracks.items[0].external_urls.spotify);
-            // console.log(response.tracks.items[0].album.name);
             for (i = 0; i < response.tracks.items.length; i++) {
                 console.log("Artist(s): " + response.tracks.items[i].artists[i].name);
                 console.log("Song Name: " + response.tracks.items[i].name);
@@ -92,10 +257,6 @@ if (command === "concert-this") {
     
             })
             .then(function(response) {
-                // console.log(response.tracks.items[0].artists[0].name);
-                // console.log(response.tracks.items[0].name);
-                // console.log(response.tracks.items[0].external_urls.spotify);
-                // console.log(response.tracks.items[0].album.name);
                 for (i = 0; i < response.tracks.items.length; i++) {
                     console.log("-------------------------");
                     console.log("Artist(s): " + response.tracks.items[i].artists[0].name);
@@ -111,7 +272,6 @@ if (command === "concert-this") {
 
       
 } else if (command === "movie-this") {
-    var nodeArgs = process.argv;
     var movieTitle = "";
 
     for (var i = 3; i < nodeArgs.length; i++) {
@@ -179,17 +339,21 @@ if (command === "concert-this") {
                 });
     }
 } else if (command === "do-what-it-says") {
+    var fs = require("fs");
 
+    fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+        return console.log(error);
+    } else {
+        var dataArr = data.split(",");
+        var argv2 = dataArr[0];
+        var argv3 = dataArr[1];
+        readRandom(argv2, argv3);
+        // console.log("node " + "liri.js " + process.argv[2] + " " + process.argv[3]);
+    }
+    });
 }
 
-
-// To retrieve the data that will power this app, you'll need to send requests using the axios package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
-    // Node-Spotify-API 
-    // Axios
-        // You'll use Axios to grab data from the OMDB API and the Bands In Town API
-    // Moment
-    // DotEnv
-    // THESE HAVE ALL BEEN DOWNLOADED
 
 // Make it so liri.js can take in one of the following commands:
 // do-what-it-says
